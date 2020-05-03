e88b86b4923deb5f7383af02396debc17a7b4c15
Dacemmi2mmi2 <dshevchenko507@gmail.com>
2020-05-03 08:21:43 +0300

    was be tests and add function for make html pages

--- js/square.js
+++ js/square.js
@@ -1,11 +1,11 @@
 const htmlElements = {
-    container : document.querySelector('.container'),
+    mainContainer : document.querySelector('.container'),
     cube : document.querySelector('.cube'),
     front : document.querySelector('.front'),
     back : document.querySelector('.back'),
     right : document.querySelector('.right'),
     left : document.querySelector('.left'),
-    moveFront : document.querySelector('.front>button'),
+    logo : document.querySelector('.logo'),
     moveLeft : document.querySelector('.left>button'),
 }
 
@@ -20,13 +20,67 @@ const globalVariables = {
 }
 
 
+const createPage = function createHtmlPages(htmlPage){
+    let allElements = htmlPage.mainPage.ru.replace(/[^a-zа-я0-9'=\s-\/&#;+]/gmi, '\n').split('\n').filter(item => {return item}),
+        allElementsSplit = [],
+        parent = {},
+        parentStrig = [],
+        someEl,
+        someText,
+        rootElement,
+        addEl;
+
+    for (let i = 0; i < allElements.length; i++){
+        allElementsSplit.push(allElements[i].split(' '));
+    }
+
+    rootElement = htmlElements.front;
+    parent[0] = rootElement;
+    parentStrig.push(htmlElements.front.tagName);
+    addEl = rootElement;
+
+    for (let i = 0; i < allElementsSplit.length - 1; i++){
+        let check;
+        htmlPage.htmlElements.filter(item => {return item === allElementsSplit[i][0] ? check = true : ''});
+        if (check){
+            addEl = parent[Object.keys(parent)[Object.keys(parent).length - 1]];
+            someEl = document.createElement(allElementsSplit[i][0]);
+            addEl.appendChild(someEl);
+            parentStrig.push(allElementsSplit[i][0]);
+            parent[i] = someEl;
+            if(allElementsSplit[i].length > 1){
+                for (let j = 0; j < allElementsSplit[i].length; j++){
+                    if(allElementsSplit[i][j].match(/=/)){
+                        someEl.setAttribute(allElementsSplit[i][j].replace(/'.{1,50}'/gi, '').slice(0, -1), allElementsSplit[i][j].replace(/^.{1,50}='/gi, '').slice(0, -1));
+                    }
+                }
+            }
+        }else{
+            if(allElementsSplit[i][0] === '/' + parentStrig[parentStrig.length - 1]){
+                parentStrig.pop();
+                delete parent[Object.keys(parent)[Object.keys(parent).length - 1]];
+            }else{
+                if(allElementsSplit[i][0].length >= 1){
+                    for (let k = 0; k < allElementsSplit[i].length; k++){
+                        someText = document.createTextNode(allElementsSplit[i][k] + ' ');
+                        parent[Object.keys(parent)[Object.keys(parent).length - 1]].appendChild(someText);
+                    }
+                }
+            }
+        }
+    }
+    rootElement.appendChild(parent[Object.keys(parent)[Object.keys(parent).length - 1]]);
+}
+fetch('js/pages.json').then((response) => {return response.json()}).then((data) => {createPage(data)});
+
+
 const widthwindow = window.addEventListener('resize', () => {
     sizeScreen(window.innerWidth);
 });
 
 
 const sizeScreen = function sizeWidthScreen(width){
-    htmlElements.container.style.perspective = width + 'px';
+    htmlElements.mainContainer.style.perspective = width + 'px';
     htmlElements.back.style.transform =  `rotateY(180deg) translateZ(${width}px)`;
     htmlElements.left.style.transform =  `rotateY(-90deg) translateZ(${width / 2}px) translateX(-${width / 2}px)`;
     htmlElements.right.style.transform =  `rotateY(90deg) translateZ(${width / 2}px) translateX(${width / 2}px)`;
@@ -34,7 +88,7 @@ const sizeScreen = function sizeWidthScreen(width){
 sizeScreen(window.innerWidth);
 
 
-const move = function moveSquare(paramCall){
+const move = function moveSquare(){
     let variables = {
         rotateX : 0,
         rotateY : 0,
@@ -44,22 +98,23 @@ const move = function moveSquare(paramCall){
         translateY : 0,
         paramWidthScreen : 0,
     }
+    let width = window.innerWidth;
 
-    window.innerWidth >= 1800 ? variables.paramWidthScreen = 20 : '';
-    window.innerWidth >= 1500 && window.innerWidth < 1800 ? variables.paramWidthScreen = 10 : '';
-    window.innerWidth >= 900 && window.innerWidth < 1500 ? variables.paramWidthScreen = 7 : '';
-    window.innerWidth >= 600 && window.innerWidth < 900 ? variables.paramWidthScreen = 5 : '';
-    window.innerWidth < 600 ? variables.paramWidthScreen = 3 : '';
+    width >= 1800 ? variables.paramWidthScreen = 20 : '';
+    width >= 1500 && width < 1800 ? variables.paramWidthScreen = 10 : '';
+    width >= 900 && width < 1500 ? variables.paramWidthScreen = 7 : '';
+    width >= 600 && width < 900 ? variables.paramWidthScreen = 5 : '';
+    width < 600 ? variables.paramWidthScreen = 3 : '';
 
     function moveAxisZ(){
         let moveZ = setInterval(() => {
-            paramsSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
-            if(variables.translateZ === -window.innerWidth){
+            paramsGlobalSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
+            if(variables.translateZ === -width){
                 clearInterval(moveZ);
                 rotateSquare();
             }else{
                 variables.translateZ -= variables.paramWidthScreen;
-                window.innerWidth + variables.translateZ > variables.paramWidthScreen ? '' : variables.translateZ = -window.innerWidth;
+                width + variables.translateZ > variables.paramWidthScreen ? '' : variables.translateZ = -width;
             }
         }, 1);
     }
@@ -67,9 +122,9 @@ const move = function moveSquare(paramCall){
 
     function rotateSquare(){
         let rotate = setInterval(() => {
-            paramsSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
-            variables.translateZ += window.innerWidth * 1.5 / 90;
-            variables.translateX += window.innerWidth * 1.5 / 90;
+            paramsGlobalSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
+            variables.translateZ += width * 1.5 / 90;
+            variables.translateX += width * 1.5 / 90;
             variables.rotateY ++ ;
             if(variables.rotateY === 90){
                 clearInterval(rotate);
@@ -80,24 +135,44 @@ const move = function moveSquare(paramCall){
 
     function moveAxisX(){
         let moveX = setInterval(() => {
-            paramsSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
-            if(variables.translateX === window.innerWidth / 2){
+            paramsGlobalSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
+            if(variables.translateX === width / 2){
                 clearInterval(moveX);
                 for (let item in variables) {
                     variables[item] = 0;
                 }
-                paramsSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
+                paramsGlobalSquare(variables.rotateY, variables.rotateX, variables.rotateZ, variables.translateY, variables.translateX, variables.translateZ);
             }else{
                 variables.translateX -= variables.paramWidthScreen;
-                variables.translateX - window.innerWidth / 2 < variables.paramWidthScreen ? variables.translateX = window.innerWidth / 2 : '';
+                variables.translateX - width / 2 < variables.paramWidthScreen ? variables.translateX = width / 2 : '';
             }
         }, 1);
     }
 }
 
-
-const paramsSquare = function applyParamsSquare(rotateY, rotateX, rotateZ, translateY, translateX, translateZ){
+const paramsGlobalSquare = function applyParamsGlobalSquare(rotateY, rotateX, rotateZ, translateY, translateX, translateZ){
     htmlElements.cube.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) rotateZ(${rotateZ}deg) translateY(${translateY}px) translateX(${translateX}px) translateZ(${translateZ}px)`;
 }
 
-htmlElements.moveFront.addEventListener('click', () => {move('click')});
+htmlElements.mainContainer.addEventListener('click', (ev) => {
+    if(ev.target.closest('main')){
+        ev.target.className === 'textLogo' ? move() : '';
+    }
+});
+
+
+
+
+
+// document.querySelector('.changeLangMainPage .option:first-child').addEventListener('click', () => {
+//     let link = 'js/pages.json';
+//     fetch(link).then((response) => {return response.json()}).then((data) => {changeLang(data)});
+// });
+
+// const changeLang = function changeLanguege(paramLangPage){
+//     console.log(paramLangPage.mainPage.ru);
+//     while(htmlElements.front.firstChild){
+//         htmlElements.front.removeChild(htmlElements.front.firstChild);
+//     }
+//     htmlElements.front.innerHTML = paramLangPage.mainPage.ru;
+// }
